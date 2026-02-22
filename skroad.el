ec3444a3291911c04e387fa441b837fe8a134416
stas <stas@loper-os.org>
2026-02-22 11:16:47 -0500

    misc

--- skroad.el
+++ skroad.el
@@ -635,8 +635,8 @@ If FINAL is t, the count sum going below zero will signal an error."
         (when final (error "Index underflow!"))))))
 
 (defun skroad--buf-indices-update (&optional disable-actions)
-  "Apply pending update to the text type indices of the current node.
-When not cached, perform initial scan (rebuild indices using buffer contents.)
+  "Apply pending update to the cached text type indices of the current node.
+If no valid cache entry found, perform initial scan (reindex buffer contents.)
 Type actions (perform for given text type, unless `DISABLE-ACTIONS` is t) :
 `on-create`: a particular payload of this type first appeared in the buffer.
 `on-init`: same as above, but during initial scan.
