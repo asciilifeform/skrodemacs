85e0612f5a20f78cb3d64fed2e347932bab63142
stas <stas@loper-os.org>
2026-04-14 10:59:38 -0400

    allow recaptioning naked urls.

--- skroad.el
+++ skroad.el
@@ -1560,7 +1560,7 @@ Return the new position if the jump actually happened; otherwise nil."
 (defun skroad--url-renamer-validate (_current proposed)
   "Determine whether a URL may be recaptioned to PROPOSED."
   (cond ((string-empty-p (skroad--clean-whitespace proposed))
-         (skroad--info "Blank caption is not allowed!")
+         (skroad--info "Caption may not be empty!")
          nil)
         (t t)))
 
